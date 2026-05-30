c4f37b58c227f600fe0c763e2257ebd4ed9120ee
Nabonita Roy <nabonitaroy15345@gmail.com>
2026-05-30 21:45:36 +0530

    Time: 39 ms (48.68%) | Memory: 31.4 MB (44.30%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,5 +4,5 @@ class Solution:
         global_max=nums[0]
         for i in range(1,len(nums)):
             current_max=max(nums[i],current_max + nums[i])
-            global_max=max(current_max , global_max)
+            global_max=max(global_max , current_max)
         return global_max
